e2c8379c043a140e719334bae474469d99e066e4
DIJOUX <emmanuel.dijoux14@gmail.com>
2017-06-01 09:22:36 +0400

    réparation de la configuration

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -4,6 +4,11 @@ namespace AppBundle\Controller;
 
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\View;
+use AppBundle\Entity\Article;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ArticleController
 {
@@ -18,5 +23,16 @@ class ArticleController
     public function showAction(Article $article)
     {
         return $article;
+    }
+
+
+    /**
+     * @Rest\Post("/articles")
+     * @Rest\View
+     * @ParamConverter("article", converter="fos_rest.request_body")
+     */
+    public function createAction(Article $article)
+    {
+        dump($article); die;
     }
 }
