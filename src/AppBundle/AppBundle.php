b2216b9c26773cac98ee6dfc3abb8378ad343ad5
DIJOUX <emmanuel.dijoux14@gmail.com>
2017-06-03 20:46:06 +0200

    Gestion d'erreurs via un listener sur kernel.exception

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -3,7 +3,13 @@
 namespace AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use AppBundle\DependencyInjection\Compiler\ExceptionNormalizerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class AppBundle extends Bundle
 {
+	public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new ExceptionNormalizerPass());
+    }
 }
